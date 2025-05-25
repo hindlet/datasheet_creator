d066de387f13c0d0287c91e4225484ec14891d46
hindlet <thomas@familyhindle.net>
2025-05-25 01:17:28 +0100

    pdfprinting working now

--- src/export/pdf.rs
+++ src/export/pdf.rs
@@ -1,16 +1,26 @@
-use std::{collections::BTreeMap, fs::write, path::PathBuf};
+use std::{fs::write, path::PathBuf};
 
-use printpdf::{GeneratePdfOptions, Mm, PdfDocument, PdfSaveOptions, XmlRenderOptions};
+use headless_chrome::{types::PrintToPdfOptions, Browser, LaunchOptions};
 use tera::Tera;
-
 use crate::data::Unit;
+use anyhow::Result;
 
 
 
+pub fn export_to_pdf(unit: &Unit, template: &Tera, path: PathBuf) -> Result<()>  {
+    let context = unit.get_context();
+    
+    let rendered = template.render("datasheet", &context).unwrap();
 
-pub fn export_to_pdf(unit: &Unit, template: &Tera, path: PathBuf) -> Result<(), std::io::Error>  {
-    let document = PdfDocument::new(&unit.name);
 
-
+    let browser = Browser::new(LaunchOptions::default()).unwrap();
+    let tab = browser.new_tab()?;
+    tab.navigate_to(format!("data:text/html;charset=utf-8,{}", rendered).as_str())?;
+    let bytes = tab.print_to_pdf(Some(PrintToPdfOptions {
+        print_background: Some(true),
+        ..Default::default()
+    })).unwrap();
+    
+    write(path, bytes)?;
     Ok(())
 }
